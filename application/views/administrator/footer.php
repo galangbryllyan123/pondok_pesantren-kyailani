b38b971e459720acc3f9a58959ea6af840d8fcd7
Galang Bryllyan Wicaksono <48407794+galangbryllyan123@users.noreply.github.com>
2023-03-14 04:28:32 +0700

    senokidal
    framework ci generasi 4

---
+++ application/views/administrator/footer.php
@@ -0,0 +1,3 @@
+
+<strong>Copyright &copy; 2016 - <?php echo date('Y'); ?> <a target='_BLANK' href="http://senoajakyuk.skom.id"> PONDOK PESANTREN
+DARUL KAILANI PURWODADI</a>.</strong> All rights reserved. 
